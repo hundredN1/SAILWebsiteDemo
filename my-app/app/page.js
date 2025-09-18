35691e2f97c71a792d42a03320e8ef7ffe5f5b3c
YiXunLim <ahundrednone101@gmail.com>
2025-09-18 00:13:17 -0500

    Create navigations and content for website

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -3,93 +3,43 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <>
+    {/* NavBar */}
+    <div className={styles.navBar}>
+      <span className={styles.logo}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/logo.png"
+          alt="Logo"
+          width={50}
+          height={50}
         />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      </span>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <span className={styles.headerText}>
+        <h1>Swine Applied Innovations Lab</h1>
+      </span>
+
+      <span className={styles.quickAccess}>
+        <button>About Us</button>
+        <button>Contact Us</button>
+        <button>Blog</button>
+        <button>Projects</button>
+      </span>
     </div>
+
+    {/* Body */}
+      <div className={styles.body}>
+        <h1 className={styles.goal}>Our Goal</h1>
+        <br></br>
+        <p className={styles.goalDescription}>To <strong>develop</strong> innovative solutions to complex questions by <strong>applying</strong> scientific knowledge and <strong>implementing</strong> emerging technologies.</p>
+      </div>
+
+
+
+    {/* Footer */}
+    <div className={styles.footer}>
+
+    </div>
+    </>
   );
 }
